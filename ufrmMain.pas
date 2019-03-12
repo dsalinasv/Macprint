00196d775a54f033ff66f49511e576b505a036a4
Daniel <daniel@macropro.com.mx>
2019-03-12 10:21:10 -0700

    agregue hoja

--- ufrmMain.pas
+++ ufrmMain.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FolderMon, Vcl.StdCtrls, Vcl.Menus,
-  Vcl.ExtCtrls, System.Actions, Vcl.ActnList;
+  Vcl.ExtCtrls, System.Actions, Vcl.ActnList, Vcl.Imaging.pngimage;
 
 type
   TfrmMain = class(TForm)
